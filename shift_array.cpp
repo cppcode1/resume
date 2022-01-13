b1ab111b20b99df1ffd83f68a1785ff1b36dc32a
Oleksandr Chastukhin <oleksandr.chastukhin@nokia.com>
2022-01-13 15:57:07 +0100

    fox the wrong description

--- shift_array.cpp
+++ shift_array.cpp
@@ -1,5 +1,5 @@
 // copyright 2022 Oleksandr Chastukhin
-// the goal is to move the wrong elements -1 to the right and keep the order of good elements
+// the goal is to move the wrong elements -1 to the left and keep the order of good elements
 
 #include <iostream>
 #include <vector>
